8206d944778effa56e2e64a013fc7f269d422e27
OlekZima <zimenkoalex34@gmail.com>
2023-04-26 09:47:15 +0200

    asdsadsadsadsad

---
+++ Lab07 - klasy/homework/script2.py
@@ -0,0 +1,3 @@
+"""
+Napisz skrypt z implementacją algorytmu Dijkstra.Skrypt ma korzystać z klas.Zmodyfikuj kod z zadania wykonanego na jednych z poprzednich zajęć.
+"""
---
+++ Lab08 - moduly/homework/script3.py
@@ -0,0 +1,6 @@
+"""
+Napisz skrypt, który będzie działał jak prosta baza produktów spożywczych.
+Skrypt ma korzytać z klas.
+Zmodyfikuj kod z zadania wykonanego na jednych z poprzednich zajęć.
+"""
+
