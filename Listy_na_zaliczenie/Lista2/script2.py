539c5880e483b331269a4f205bb53acad24fcb76
Olek Zima <zimenkoalex34@gmail.com>
2023-05-26 15:07:28 +0200

    Second task improved

--- Listy_na_zaliczenie/Lista2/script2.py
+++ Listy_na_zaliczenie/Lista2/script2.py
@@ -2,28 +2,32 @@ from math import sqrt
 
 
 def main():
-    num1 = int(input("Podaj pierwszą liczbę: "))
-    num2 = int(input("Podaj pierwszą liczbę: "))
+    flag = False
+    while not flag:
+        num1 = int(input("Podaj pierwszą liczbę: "))
+        num2 = int(input("Podaj pierwszą liczbę: "))
 
-    operation = input("Podaj działanie, +, -, *, /, %, sqrt, anyroot, ^: ")
+        operation = input("Podaj działanie, +, -, *, /, %, sqrt, anyroot, ^ albo q, by wyjść: ")
 
-    if operation == "+":
-        print(f"Wynikiem działania {num1} {operation} {num2} jest {num1 + num2}")
-    elif operation == "-":
-        print(f"Wynikiem działania {num1} {operation} {num2} jest {num1 - num2}")
-    elif operation == "*":
-        print(f"Wynikiem działania {num1} {operation} {num2} jest {num1 * num2}")
-    elif operation == "/":
-        print(f"Wynikiem działania {num1} {operation} {num2} jest {num1 / num2}")
-    elif operation == "%":
-        print(f"Wynikiem działania {num1} {operation} {num2} jest {num1 % num2}")
-    elif operation == "sqrt":
-        print(f"Wynikiem działania {operation}({num1}) jest {num1 ** (1/2)}")
-        print(f"Wynikiem działania {operation}({num2}) jest {num2 ** (1/2)}")
-    elif operation == "anyroot":
-        print(f"Wynikiem działania {num1}_root({num2}) jest {num2 ** (1 / num1)}")
-    elif operation == "^":
-        print(f"Wynikiem działania {num1} {operation} {num2} jest {num1 ** num2}")
+        if operation == "+":
+            print(f"Wynikiem działania {num1} {operation} {num2} jest {num1 + num2}")
+        elif operation == "-":
+            print(f"Wynikiem działania {num1} {operation} {num2} jest {num1 - num2}")
+        elif operation == "*":
+            print(f"Wynikiem działania {num1} {operation} {num2} jest {num1 * num2}")
+        elif operation == "/":
+            print(f"Wynikiem działania {num1} {operation} {num2} jest {num1 / num2}")
+        elif operation == "%":
+            print(f"Wynikiem działania {num1} {operation} {num2} jest {num1 % num2}")
+        elif operation == "sqrt":
+            print(f"Wynikiem działania {operation}({num1}) jest {num1 ** (1 / 2)}")
+            print(f"Wynikiem działania {operation}({num2}) jest {num2 ** (1 / 2)}")
+        elif operation == "anyroot":
+            print(f"Wynikiem działania {num1}_root({num2}) jest {num2 ** (1 / num1)}")
+        elif operation == "^":
+            print(f"Wynikiem działania {num1} {operation} {num2} jest {num1 ** num2}")
+        elif operation == "q":
+            break
 
 
 if __name__ == "__main__":
