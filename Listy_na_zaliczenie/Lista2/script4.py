9fa00e606ef0dea763463bcef9d5e628e083d695
Olek Zima <zimenkoalex34@gmail.com>
2023-05-26 18:16:21 +0200

    token generator changed

--- Listy_na_zaliczenie/Lista2/script4.py
+++ Listy_na_zaliczenie/Lista2/script4.py
@@ -1,48 +1,78 @@
 import datetime
+import uuid
 
 
-# Funkcja do generowania unikalnego tokena
 def generate_token(first_name, last_name, email, expiration_date):
-    # Kombinacja danych użytkownika w celu utworzenia ciągu
     data = first_name + last_name + email + expiration_date
-
-    # Generowanie unikalnego tokenu na podstawie ciągu danych
-    token = ""
-    for char in data:
-        token += str(ord(char))
-
+    token = str(uuid.uuid5(uuid.NAMESPACE_DNS, data))
     return token
 
 
-# Funkcja do weryfikacji tokena
 def verify_token(token, first_name, last_name, email, expiration_date):
-    # Generowanie oczekiwanego tokenu na podstawie podanych danych
     expected_token = generate_token(first_name, last_name, email, expiration_date)
-
-    # Porównanie podanego tokenu z oczekiwanym tokenem
-    if token == expected_token:
-        return True
-    else:
-        return False
+    return token == expected_token
 
 
-# Przykładowe dane użytkownika
-first_name = input("Podaj imię: ")
-last_name = input("Podaj nazwisko: ")
-email = input("Podaj adres e-mail: ")
-expiration_date = input("Podaj datę ważności tokena (RRRR-MM-DD): ")
+def format_date(date):
+    return datetime.datetime.strptime(date, "%Y-%m-%d").strftime("%d.%m.%Y")
 
-# Wygenerowanie tokenu
-token = generate_token(first_name, last_name, email, expiration_date)
-print("Wygenerowany token:", token)
 
-# Weryfikacja tokenu
-user_token = input("Podaj token do weryfikacji: ")
-is_valid = verify_token(user_token, first_name, last_name, email, expiration_date)
+def print_header():
+    print("=== Token Authentication ===")
 
-# Sprawdzenie ważności tokenu
-if is_valid:
+
+def print_token(token):
+    print("Wygenerowany token:", token)
+
+
+def print_access_granted():
     print("Token jest ważny. Dostęp przyznany.")
-    # Tutaj można użyć tokenu do uzyskania danych użytkownika
-else:
+
+
+def print_access_denied():
     print("Token jest nieprawidłowy. Dostęp zabroniony.")
+
+
+def input_user_data():
+    first_name = input("Podaj imię: ")
+    last_name = input("Podaj nazwisko: ")
+    email = input("Podaj adres e-mail: ")
+    expiration_date = input("Podaj datę ważności tokena (RRRR-MM-DD): ")
+    return first_name, last_name, email, expiration_date
+
+
+def input_token():
+    return input("Podaj token do weryfikacji: ")
+
+
+def print_user_data(user_data):
+    print("Dane użytkownika:")
+    print("Imię:", user_data['first_name'])
+    print("Nazwisko:", user_data['last_name'])
+    print("E-mail:", user_data['email'])
+    print("Data ważności tokena:", format_date(user_data['expiration_date']))
+
+
+def print_menu():
+    print("Wybierz opcję:")
+    print("1. Wygeneruj token")
+    print("0. Wyjdź")
+
+
+def main():
+    print_header()
+    while True:
+        print_menu()
+        choice = input("Twój wybór: ")
+        if choice == "1":
+            first_name, last_name, email, expiration_date = input_user_data()
+            token = generate_token(first_name, last_name, email, expiration_date)
+            print_token(token)
+        elif choice == "0":
+            break
+        else:
+            print("Nieprawidłowy wybór. Spróbuj ponownie.")
+
+
+if __name__ == "__main__":
+    main()
